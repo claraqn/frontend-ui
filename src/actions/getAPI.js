1c587c7b177da81408e947d1dec3bb285c3ec9c6
alzseven <jhp7140@gmail.com>
2021-05-01 20:46:58 +0900

    Create Actions

---
+++ src/actions/getAPI.js
@@ -0,0 +1,40 @@
+import axios from 'axios';
+
+const GET_DATA_PENDING = 'GET_DATA_PENDING';
+const GET_DATA_SUCCESS = 'GET_DATA_SUCCESS';
+const GET_DATA_FAILURE = 'GET_DATA_FAILURE';
+
+function fetchData(param) {
+  // TODO: Fix as env value
+  return axios.get(`https://alzseven.github.io/api/mock/${param}`);
+}
+
+function getAPI(param) {
+  return (dispatch) => {
+    fetchData(param)
+      .then((response) => {
+        console.log(response);
+        dispatch({
+          type: GET_DATA_SUCCESS,
+          payload: response,
+        });
+      })
+      .catch((error) => {
+        dispatch({
+          type: GET_DATA_FAILURE,
+          payload: error,
+        });
+      });
+  };
+}
+
+//---
+
+function getStations() {
+  return (dispatch) => {
+    dispatch({ type: GET_DATA_PENDING });
+    dispatch(getAPI('stations.json'));
+  };
+}
+
+export { GET_DATA_PENDING, GET_DATA_SUCCESS, GET_DATA_FAILURE, getStations };
