d13ff659163ac73832bf0c2356f647b1edcfb58b
alzseven <jhp7140@gmail.com>
2021-05-30 03:14:45 +0900

    Handle request error & loading

--- src/pages/stationlists/StationList.js
+++ src/pages/stationlists/StationList.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Row, Col, Table, Button } from 'reactstrap';
+import { Row, Col, Table, Button, Spinner } from 'reactstrap';
 import Widget from '../../components/Widget';
 import s from './StationList.module.scss';
 import uuid from 'uuid/v4';
@@ -7,6 +7,7 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import * as apiActions from '../../actions/getAPI';
 import StationItem from './StationItem';
+import Error from '../error/ErrorPage';
 
 class StationLists extends React.Component {
   refreshStationList() {
@@ -20,60 +21,72 @@ class StationLists extends React.Component {
 
   render() {
     const { stations, error, loading } = this.props;
+    if (loading === true) {
+      return (
+        <div className={s.loading}>
+          <Spinner color="light" />
+        </div>
+      );
+    }
+    if (error === false) {
+      return (
+        <div className={s.root}>
+          <h1 className="page-title">
+            Parking Lot <span className="fw-semi-bold">List</span>
+            <small className={s.small}> 주차장 목록을 확인합니다.</small>
+          </h1>
 
-    return (
-      <div className={s.root}>
-        <h1 className="page-title">
-          Parking Lot <span className="fw-semi-bold">List</span>
-          <small className={s.small}> 주차장 목록을 확인합니다.</small>
-        </h1>
-
-        <Row>
-          <Col>
-            <Widget
-              title={
-                <h5>
-                  제주대학교 내 주차장 목록
-                  <span className="fw-semi-bold"></span>
-                </h5>
-              }
-              bodyClass={s.mainTableWidget}
-            >
-              <Button
-                outline
-                className={s.button}
-                onClick={this.refreshStationList.bind(this)}
+          <Row>
+            <Col>
+              <Widget
+                title={
+                  <h5>
+                    제주대학교 내 주차장 목록
+                    <span className="fw-semi-bold"></span>
+                  </h5>
+                }
+                bodyClass={s.mainTableWidget}
               >
-                Refresh
-              </Button>
-              <Table striped className={s.table}>
-                <thead>
-                  <tr className="fs-sm">
-                    <th className="hidden-sm-down">#</th>
-                    <th>Picture</th>
-                    <th>Name</th>
-                    <th className="hidden-sm-down">Location</th>
-                    <th className="hidden-sm-down">Number of parking spaces</th>
-                    <th className="hidden-sm-down">Favorite</th>
-                    <th className="hidden-sm-down">
-                      Check the number of vacancy
-                    </th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {stations.map((station) => (
-                    <StationItem
-                      key={`${station.id}${uuid()}`}
-                      {...station}
-                    ></StationItem>
-                  ))}
-                </tbody>
-              </Table>
-            </Widget>
-          </Col>
-        </Row>
-      </div>
-    );
+                <Button
+                  outline
+                  className={s.button}
+                  onClick={this.refreshStationList.bind(this)}
+                >
+                  Refresh
+                </Button>
+                <Table striped className={s.table}>
+                  <thead>
+                    <tr className="fs-sm">
+                      <th className="hidden-sm-down">#</th>
+                      <th>Picture</th>
+                      <th>Name</th>
+                      <th className="hidden-sm-down">Location</th>
+                      <th className="hidden-sm-down">
+                        Number of parking spaces
+                      </th>
+                      <th className="hidden-sm-down">Favorite</th>
+                      <th className="hidden-sm-down">
+                        Check the number of vacancy
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {stations.map((station) => (
+                      <StationItem
+                        key={`${station.id}${uuid()}`}
+                        {...station}
+                      ></StationItem>
+                    ))}
+                  </tbody>
+                </Table>
+              </Widget>
+            </Col>
+          </Row>
+        </div>
+      );
+    } else {
+      return <Error />;
+    }
   }
 }
 
--- src/pages/typography/Typography.js
+++ src/pages/typography/Typography.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Row, Col, Table, Button } from 'reactstrap';
+import { Row, Col, Table, Button, Spinner } from 'reactstrap';
 import Widget from '../../components/Widget';
 import s from './Typography.module.scss';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import * as apiActions from '../../actions/getAPI';
 import StationItem from '../stationlists/StationItem';
-
+import Error from '../error/ErrorPage';
 class Typography extends React.Component {
   refreshStationList() {
     this.props.APIActions.getStations();
@@ -33,62 +33,81 @@ class Typography extends React.Component {
     const { stations, error, loading } = this.props;
     const favorites = this.getCookie('favorites');
 
-    return (
-      <div className={s.root}>
-        <h1 className="page-title">
-          <span className="fw-semi-bold">Favorites</span> Parking Lot{' '}
-          <span className="fw-semi-bold">List</span>
-          <small className={s.small}> 즐겨찾는 주차장 목록을 확인합니다.</small>
-        </h1>
-        <Row>
-          <Col>
-            <Widget
-              title={
-                <h5>
-                  제주대학교 내 주차장 목록
-                  <span className="fw-semi-bold"></span>
-                </h5>
-              }
-              bodyClass={s.mainTableWidget}
-            >
-              <Button
-                outline
-                className={s.button}
-                onClick={this.refreshStationList.bind(this)}
+    if (loading === true) {
+      return (
+        <div className={s.loading}>
+          <Spinner color="light" />
+        </div>
+      );
+    }
+    if (error === false) {
+      return (
+        <div className={s.root}>
+          <h1 className="page-title">
+            <span className="fw-semi-bold">Favorites</span> Parking Lot{' '}
+            <span className="fw-semi-bold">List</span>
+            <small className={s.small}>
+              {' '}
+              즐겨찾는 주차장 목록을 확인합니다.
+            </small>
+          </h1>
+          <Row>
+            <Col>
+              <Widget
+                title={
+                  <h5>
+                    제주대학교 내 주차장 목록
+                    <span className="fw-semi-bold"></span>
+                  </h5>
+                }
+                bodyClass={s.mainTableWidget}
               >
-                Refresh
-              </Button>
+                <Button
+                  outline
+                  className={s.button}
+                  onClick={this.refreshStationList.bind(this)}
+                >
+                  Refresh
+                </Button>
 
-              <Table striped>
-                <thead>
-                  <tr className="fs-sm">
-                    <th className="hidden-sm-down">#</th>
-                    <th>Picture</th>
-                    <th>Name</th>
-                    <th className="hidden-sm-down">Location</th>
-                    <th className="hidden-sm-down">Number of parking spaces</th>
-                    <th className="hidden-sm-down">Favorite</th>
-                    <th className="hidden-sm-down">
-                      Check the number of vacancy
-                    </th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {stations.map((station) =>
-                    favorites !== undefined &&
-                    favorites.indexOf(station.id) > -1 ? (
-                      <StationItem key={station.id} {...station}></StationItem>
-                    ) : (
-                      ''
-                    ),
-                  )}
-                </tbody>
-              </Table>
-            </Widget>
-          </Col>
-        </Row>
-      </div>
-    );
+                <Table striped>
+                  <thead>
+                    <tr className="fs-sm">
+                      <th className="hidden-sm-down">#</th>
+                      <th>Picture</th>
+                      <th>Name</th>
+                      <th className="hidden-sm-down">Location</th>
+                      <th className="hidden-sm-down">
+                        Number of parking spaces
+                      </th>
+                      <th className="hidden-sm-down">Favorite</th>
+                      <th className="hidden-sm-down">
+                        Check the number of vacancy
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {stations.map((station) =>
+                      favorites !== undefined &&
+                      favorites.indexOf(station.id) > -1 ? (
+                        <StationItem
+                          key={station.id}
+                          {...station}
+                        ></StationItem>
+                      ) : (
+                        ''
+                      ),
+                    )}
+                  </tbody>
+                </Table>
+              </Widget>
+            </Col>
+          </Row>
+        </div>
+      );
+    } else {
+      return <Error />;
+    }
   }
 }
 
